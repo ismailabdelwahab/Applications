097c9bd55624a559b5cf9858a72d432e27c351be
ismailAbdelwahab <is.abdelwahab@gmail.com>
2020-08-21 21:41:13 +0200

    Adding a visual sorting application

---
+++ sorter/View/Application.py
@@ -0,0 +1,103 @@
+try:
+	import tkinter as tk
+except ImportError:
+	import Tkinter as tk
+import threading
+from random import shuffle
+
+from Model.Sorting_algorithms.Bubble_sort import bubble_sort
+from Model.Sorting_algorithms.Quick_sort import quick_sort
+
+class Application(tk.Frame):
+	WINDOW_DIMENSION = "1000x420"
+	CANVAS_WIDTH = 600
+	CANVAS_HEIGHT = 400
+	CANVAS_BG = "black"
+	def __init__(self, array=None):
+		self.array = array
+		self.create_master_window()
+		self.create_canvas(); self.fill_canvas()
+		self.create_buttons()
+
+	def redraw(self, i=-1, j=-1):
+		self.clear_canvas(); self.fill_canvas(i, j)
+		
+	def fill_canvas(self, i=-1, j=-1):
+		X_PADDING = 3 ; Y_PADDING = 5
+		c_width = int(self.canvas['width']) ; c_heigth = int(self.canvas['height']) - Y_PADDING
+		block_width = c_width / len(self.array) ; block_heigth = c_heigth / max(self.array)
+
+		for index, num in enumerate(self.array):
+			color = 'white' if index != i else 'red'
+			color = color if index != j else 'green'
+			x1 = X_PADDING + index * block_width      ; y1 = c_heigth - Y_PADDING - num*block_heigth
+			x2 = X_PADDING + (index+1) * block_width  ; y2 = c_heigth - Y_PADDING
+			self.canvas.create_rectangle(x1,y1, x2,y2, fill=color)
+
+	def clear_canvas(self):
+		self.canvas.delete('all')
+
+	def shuffle_array(self):
+		shuffle(self.array)
+		self.redraw()
+
+	def update_array_len(self, n):
+		self.array = [x for x in range(n)]
+		self.redraw()
+
+############# Creating stuff ######################################################################################
+	def create_master_window(self):
+		self.master = tk.Tk()
+		self.master.geometry( self.WINDOW_DIMENSION )
+		self.master.configure(bg="gray76")
+		super().__init__(self.master)
+		self.grid()
+
+	def create_canvas(self):
+		self.canvas = tk.Canvas(self.master, bg=self.CANVAS_BG, width=self.CANVAS_WIDTH, height=self.CANVAS_HEIGHT)
+		self.canvas.grid(row=0, column=1)
+
+	def create_buttons(self):
+		left_pannel = tk.Frame(self)
+		left_pannel.grid(row=0,column=0, sticky="n")
+		####################################################### ROW = 0
+		nb_elem_label = tk.Label(left_pannel, text="Number of elements:")
+		nb_elem_label.grid(row=0, column=0)
+
+		nb = tk.StringVar()
+		nb.set(str( len(self.array) ))
+		self.nb_elem_entry = tk.Entry(left_pannel, textvariable=nb, justify="center", width=6)
+		self.nb_elem_entry.grid(row=0, column=1)
+		
+		nb_elem_button = tk.Button(left_pannel, text="Edit changes", fg="black", 
+			command=lambda: self.update_array_len( int(self.nb_elem_entry.get()) ))
+		nb_elem_button.grid(row=0, column=2)
+
+		####################################################### ROW = 1
+		fill_canvas_button = tk.Button(left_pannel, text="Show bars", fg="green",command=self.fill_canvas)
+		fill_canvas_button.grid(row=1, column=0)
+		clear_canvas_button = tk.Button(left_pannel, text="Hide bars", fg="red",command=self.clear_canvas)
+		clear_canvas_button.grid(row=1, column=1)
+
+		suffle_array_button = tk.Button(left_pannel, text="Shuffle the array", fg="black",command=self.shuffle_array)
+		suffle_array_button.grid(row=1, column=2)
+		####################################################### ROW = 2
+
+		####################################################### ROW = 3 - 5
+		### O( n² ) Algos:
+		n_squared = tk.Label( left_pannel, text="O(n²) algorithms:" )
+		n_squared.grid(row=3, column=0)
+		bubble_sort_button= tk.Button(left_pannel, text="Bubble sort", fg="purple",
+			command= lambda : threading.Thread(target=bubble_sort, args=[self, self.array]).start())
+		bubble_sort_button.grid(row=4, column=0)
+
+		####################################################### ROW = 6 - 8
+		### O( n*log(n) ) Algos:
+		n_log_n = tk.Label( left_pannel, text="O(n*log(n)) algorithms:" )
+		n_log_n.grid(row=6, column=0)
+		quick_sort_button= tk.Button(left_pannel, text="Quick sort", fg="purple",
+			command= lambda : threading.Thread(target=quick_sort, args=[self, self.array,0, len(self.array)-1]).start())
+		quick_sort_button.grid(row=7, column=0)
+		
+		self.quit = tk.Button(left_pannel, text="QUIT", fg="red",command=self.master.destroy)
+		self.quit.grid(row=10, column=1)
