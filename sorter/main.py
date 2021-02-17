3bb83c09231cd5dcd27fc68919744ea5861e8f87
ismailAbdelwahab <is.abdelwahab@gmail.com>
2021-02-17 10:11:33 +0100

    Cleaning repo, deleting old files.

--- sorter/main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from View.Application import Application
-
-if __name__ == '__main__':
-	NB_OF_ELEMENTS = 50
-	array = [x for x in range(0, NB_OF_ELEMENTS)]
-	app = Application( array=array )
-
-	app.mainloop()
